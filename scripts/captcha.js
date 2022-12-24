4f7db71a33d49843e7f950946e03aeb9808353ee
sanguineimpasse <10geneius@gmail.com>
2022-12-24 20:22:06 +0800

    update 3
    merry christmas!

--- scripts/captcha.js
+++ scripts/captcha.js
@@ -1,31 +1,93 @@
+window.addEventListener('load', ()=>{
+    generateCaptcha();
+    setCaptcha();
+});
+
+document.querySelector(".captcha-refresh").addEventListener("click", ()=> {
+    generateCaptcha();
+    setCaptcha();
+});
+
+let captchaValue = "";
+const fonts = ["cursive", "sans-serif", "serif", "monospace"];
+
+function generateCaptcha(){
+    let value = btoa(Math.random()*1000000000);
+    value = value.substr(0,5+Math.random()*5);
+    captchaValue = value;
+}
+
+function setCaptcha(){
+    let html = captchaValue.split("").map((char)=>{
+        const rotate = -20 + Math.trunc(Math.random()*30);
+        const font = Math.trunc(Math.random()*fonts.length);
+        return `<span 
+                        style="transform:rotate(${rotate}deg); 
+                        font-family:${fonts[font]}
+                   ">${char}</span>`;
+    }).join("");
+    document.querySelector(".captcha .preview").innerHTML = html;
+}
+
+function checkCaptcha(){
+    let userInputCaptcha = document.getElementById("captcha-form").value;
+
+    console.log('the user input was ' + userInputCaptcha);
+    console.log('the captcha text was "' + captchaValue + '"');
+
+    if(captchaValue === userInputCaptcha){
+        console.log('THE USER PASSED THE VIBE CHECK');
+    }
+    else if(captchaValue !== userInputCaptcha){
+        console.log('THE USER DID NOT PASS THE VIBE CHECK');
+        generateCaptcha();
+        setCaptcha();
+    }
+}
+
+
+/* 
+//OLD METHOD
 (function(){
     const fonts = ["cursive", "sans-serif", "serif", "monospace"];
-    let captchaValue = "";
+    
+
     function generateCaptcha(){
         let value = btoa(Math.random()*1000000000);
         value = value.substr(0,5+Math.random()*5);
         captchaValue = value;
     }
+
     function setCaptcha(){
         let html = captchaValue.split("").map((char)=>{
             const rotate = -20 + Math.trunc(Math.random()*30);
             const font = Math.trunc(Math.random()*fonts.length);
-            return `<span
-                style="
-                    transform:rotate(${rotate}deg);
-                    font-family:${fonts[font]}
-                "
-                >${char}</span>`;
+            return `<span 
+                            style="transform:rotate(${rotate}deg); 
+                            font-family:${fonts[font]}
+                    ">${char}</span>`;
         }).join("");
             document.querySelector(".captcha .preview").innerHTML = html;
     }
-        function initCaptcha(){
-            document.querySelector(".captcha .captcha-refresh").addEventListener("click",function(){
-                generateCaptcha();
-                setCaptcha();
-            });
-                generateCaptcha();
-                setCaptcha();
-        }
+    
+    function initCaptcha(){
+        document.querySelector(".captcha-refresh").addEventListener("click", ()=> {
+            generateCaptcha();
+            setCaptcha();
+        });
+        generateCaptcha();
+        setCaptcha();
+    }
     initCaptcha();
+
+    
 })();
+
+const fonts = ["cursive", "sans-serif", "serif", "monospace"];
+    
+function generateCaptcha(){
+    let value = btoa(Math.random()*1000000000);
+    value = value.substr(0,5+Math.random()*5);
+    captchaValue = value;
+} 
+*/
